1cde4137d8f3bf27de4554e9c1c84e9ebf407c9a
boratarhan <bora.tarhan@gmail.com>
2019-12-31 14:29:46 -0600

    Added a batch script for future use - it will work when I fix the argv issue.

---
+++ source_system/_run_StrategySMA.bat
@@ -0,0 +1,10 @@
+start python C:\Users\bora\Documents\GitHub\BTrade\source_system\forwarder.py 5555
+sleep 30
+start python C:\Users\bora\Documents\GitHub\BTrade\source_system\forwarder.py 5557
+sleep 30
+start python C:\Users\bora\Documents\GitHub\BTrade\source_system\portfolio.py practice 5554
+sleep 30
+start python C:\Users\bora\Documents\GitHub\BTrade\source_system\strategySMA.py EUR_USD practice S5 5556 10
+sleep 30
+start python C:\Users\bora\Documents\GitHub\BTrade\source_system\feeder.py EUR_USD practice S5 5555
+sleep 30
